dd583ea26bc329f768a96b60de505c031cf4d187
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 18:10:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,8 @@
 var playlist = new Object({"Phil Ochs":"meh"})
 function updatePlaylist(playlist, artist,song){
 return Object.assign({}, playlist, {artist:song})}
-function removeFromPlaylist(playlist,artist){return delete playlist.artist}
+function removeFromPlaylist(playlist,artist){
+  var copy = Object.assign({}, playlist)
+  copy.delete(artist)
+  return copy
+}
