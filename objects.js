5426ecb8041825fc55ee53c0cb8d034ae019743a
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 17:56:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object({LLLLartist:"song"})
+var playlist = new Object({artist:"song"})
 function updatePlaylist(playlist, artist,song){
   playlist[song]=artist; 
   return playlist}
